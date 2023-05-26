a08eb2e8261af9b6b915b89a606e03121e3a06bf
IVainqueur <ishimvainqueur@gmail.com>
2023-05-26 20:42:33 +0200

    [feat+fix] esmodules, netlify-serverless, next-upgrade

--- src/pages/api/slack/events/index.js
+++ src/pages/api/slack/events/index.js
@@ -2,6 +2,7 @@
 import { App } from '@slack/bolt'
 import { setupHandlers } from './_handleEvents'
 import { logger } from '@/utils/logger'
+
 export const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -11,8 +12,9 @@ export const app = new App({
 
 let isBoltUp = false
 
-export default async function handler(req, res) {
-    if (req.body.type === 'pointless') {
+// this is default handle for a vercel serverless function
+export default async function _handler(req, res) {
+    if (req.body.type === 'pointless request') {
         logger.info('pointless')
         return res.status(200).json({ success: true })
     }
@@ -33,5 +35,34 @@ export default async function handler(req, res) {
         ack: () => { },
     })
 
+}
+
+// this is the handler for a netlify serverless function
+export const handler = async (req, context) => {
+    if (req.body.type === 'pointless') {
+        logger.info('pointless request')
+        return { statusCode: 200, body: JSON.stringify({ success: true }), }
+    }
+    if (req.body.type === 'url_verification') {
+        logger.info('url_verification')
+        return { statusCode: 200, body: JSON.stringify({ challenge: req.body.challenge }), }
+    }
+    
+    if (!isBoltUp) {
+        await app.start()
+        isBoltUp = true
+        logger.info('⚡️ Bolt app is running!')
+        setupHandlers()
+    }
+
+    app.processEvent({
+        body: req.body,
+        ack: () => { },
+    })
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ success: true }),
+    }
 }
 
