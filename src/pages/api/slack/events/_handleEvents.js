24424e6c6f1431929395a9a5e2952d0aef588a98
IVainqueur <ishimvainqueur@gmail.com>
2023-05-26 16:40:05 +0200

    [feat] handling app mentions

--- src/pages/api/slack/events/_handleEvents.js
+++ src/pages/api/slack/events/_handleEvents.js
@@ -5,9 +5,19 @@ const NIGEN_URI = process.env.NIGEN_URI
 
 
 export function setupHandlers() {
-    app.message(/^(hello|hi|hey)/i, async ({ message, say }) => {
-        logger.info("I am being greated by", message.username)
+    app.message(/^(hello|hi|hey)$/i, async ({ message, say, }) => {
+        logger.info("I am being greated")
         await say(`Wassup, <@${message.user}>`);
+    })
+
+    app.event('app_mention', async({ body, client, say }) => {
+        logger.info("I was mentioned")
+        await client.reactions.add({
+            name: 'wave',
+            channel: body.event.channel,
+            timestamp: body.event.ts
+        })
+
     })
 
     // Snitching on deleted messages
@@ -26,19 +36,20 @@ export function setupHandlers() {
             minute: 'numeric',
             hour12: true
         })
-
-        const thread_ts = message.previous_message.thread_ts ?? message.previous_message.ts
-
-        const reply = await say({
-            text: `<!channel>, <@${message.previous_message.user}> deleted their message!`,
-            thread_ts,
-        })
+        let reply = null;
+        if (!message.previous_message.thread_ts) {
+            const thread_ts = message.previous_message.ts
+            reply = await say({
+                text: `<!channel>, <@${message.previous_message.user}> deleted their message!`,
+                thread_ts,
+            })
+        }
 
 
         const screenshotURI = `${NIGEN_URI}?message=${encodeURI(message.previous_message.text)}&username=${encodeURI(userInfo.name)}&time=${encodeURI(compositionDate)}&avatar=${userInfo.avatar}`
         await say({
-            text: ``,
-            thread_ts: reply.ts,
+            text: reply ? '' : `<!channel>, <@${message.previous_message.user}> deleted their message!`,
+            thread_ts: reply ? reply.ts : message.previous_message.thread_ts,
             attachments: [
                 {
                     fallback: "Removed message info",
